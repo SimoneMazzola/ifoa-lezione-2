4f98ab5372010758c2f6474334a39bd4f9d3e057
admin <admin@PC1955>
2017-03-28 10:01:44 +0200

    aggiunti test vari

--- ifoaLezione1-Git/src/lezione1/EmailValidator.java
+++ ifoaLezione1-Git/src/lezione1/EmailValidator.java
@@ -8,7 +8,7 @@ public class EmailValidator {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		Pattern regex = Pattern.compile("^[a-z](\\.{0,1}[a-z]{1,})*+@(\\.{0,1}[a-z]{1,})+\\.[a-z]{2,6}$");
+		Pattern regex = Pattern.compile("^[a-z | 0-9](\\.{0,1}[a-z]{1,})*+@(\\.{0,1}[a-z]{1,})+\\.[a-z]{2,6}$");
 		System.out.println("Inserisci una e-mail: ");
 		
 		String email = sc.nextLine();
